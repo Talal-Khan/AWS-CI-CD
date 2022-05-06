29c2da33e3610eedb7eb9b8730f9001338eef394
Talal-Khan <62174348+Talal-Khan@users.noreply.github.com>
2022-05-07 00:48:12 +0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,6 +6,7 @@ def lambda_handler(event, context):
     data = {
         'output': 'Hello from '+ event['Country'],
         'timestamp': datetime.datetime.utcnow().isoformat()
+        # Hello World
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
